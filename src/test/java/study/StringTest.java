8b27ccb5b4add89b83bd69e237466db2c7cf52aa
delvering17 <delvering17@gmail.com>
2024-09-20 00:29:11 +0900

    Feat: StringTest 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,7 +1,9 @@
 package study;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
@@ -10,4 +12,38 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        String input = "1,2";
+        String[] splitArr = input.split(",");
+
+        assertThat(splitArr).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String input = "(1,2)";
+        String result = input.substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt() {
+        String input = "abc";
+
+        assertThat(findChar(input, 0)).isEqualTo('a');
+        assertThat(findChar(input, 1)).isEqualTo('b');
+        assertThat(findChar(input, 2)).isEqualTo('c');
+        assertThatThrownBy(() -> {
+            assertThat(findChar(input, 3));
+        })
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+    }
+
+    private char findChar(String str, int index) {
+        return str.charAt(index);
+    }
+
 }
